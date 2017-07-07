cd9eec16ba583691e4ce216f23f154f913c396ba
Fogarasi Norbert <norbert.fogarasi.it@gmail.com>
2017-07-07 21:15:32 +0300

    Added DatePicker

--- app/src/main/java/com/lynxsolutions/norbertfogarasi/loginapp/RegisterActivity.java
+++ app/src/main/java/com/lynxsolutions/norbertfogarasi/loginapp/RegisterActivity.java
@@ -5,14 +5,16 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
+import java.util.Locale;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class RegisterActivity extends Activity {
 
+    private Calendar calendar;
     private EditText etFirstName, etLastName, etEmail, etPassword, etPasswordConfirm;
     private TextView tvDate;
 
@@ -31,17 +33,32 @@ public class RegisterActivity extends Activity {
         etPasswordConfirm = (EditText) findViewById(R.id.et_password_confirm);
         tvDate = (TextView) findViewById(R.id.tv_birthday);
 
+        //Date picker
+        calendar = Calendar.getInstance();
+
+        calendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, dayOfMonth);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateTextView();
+            }
+        };
         tvDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar calendar = Calendar.getInstance();
-                DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-
-                    }
-                };
+                new DatePickerDialog(RegisterActivity.this, date, calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+    }
+
+    private void updateTextView() {
+        //Updates the textview with the chosen date
+        String format = "yyyy.MM.dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
+        tvDate.setText(sdf.format(calendar.getTime()));
     }
 }
