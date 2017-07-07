c7025abb077c4beef3e6bd2fa79925284f7be3be
Fogarasi Norbert <norbert.fogarasi.it@gmail.com>
2017-07-07 22:29:55 +0300

    Added camera intent, image chooser intent

--- app/src/main/java/com/lynxsolutions/norbertfogarasi/loginapp/RegisterActivity.java
+++ app/src/main/java/com/lynxsolutions/norbertfogarasi/loginapp/RegisterActivity.java
@@ -2,19 +2,32 @@ package com.lynxsolutions.norbertfogarasi.loginapp;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RegisterActivity extends Activity {
 
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int SELECT_PHOTO = 1889;
+
     private Calendar calendar;
+    private ImageView imgProfile;
     private EditText etFirstName, etLastName, etEmail, etPassword, etPasswordConfirm;
     private TextView tvDate;
 
@@ -32,6 +45,25 @@ public class RegisterActivity extends Activity {
         etPassword = (EditText) findViewById(R.id.et_password);
         etPasswordConfirm = (EditText) findViewById(R.id.et_password_confirm);
         tvDate = (TextView) findViewById(R.id.tv_birthday);
+        imgProfile = (ImageView) findViewById(R.id.img_profile);
+
+        //Capture an image
+        findViewById(R.id.btn_capture).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), CAMERA_REQUEST);
+            }
+        });
+
+        //Choose an image from gallery
+        findViewById(R.id.btn_choose).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pictureIntent = new Intent();
+                pictureIntent.setType("image/*");
+                startActivityForResult(pictureIntent, SELECT_PHOTO);
+            }
+        });
 
         //Date picker
         calendar = Calendar.getInstance();
@@ -60,5 +92,31 @@ public class RegisterActivity extends Activity {
         String format = "yyyy.MM.dd";
         SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
         tvDate.setText(sdf.format(calendar.getTime()));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case CAMERA_REQUEST:
+                if(resultCode == Activity.RESULT_OK) {
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    imgProfile.setImageBitmap(photo);
+                }
+                break;
+            case SELECT_PHOTO:
+                if(resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = data.getData();
+                    try {
+                        InputStream imageStream = getContentResolver().openInputStream(selectedImage);
+                        Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
+                        imgProfile.setImageBitmap(yourSelectedImage);
+
+                    } catch(FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+        }
     }
 }
